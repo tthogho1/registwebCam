c822e700c20d8882dbd4bafb856bae84e528e430
t-tougou <tthogho1@gmail.com>
2025-01-30 19:32:32 +0900

    add image download and embedding

---
+++ util/getImage.go
@@ -0,0 +1,46 @@
+package util
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"strconv"
+)
+
+func GetImage(downloadUrl string) (data []byte) {
+
+	// リクエストを作成
+	req, err := http.NewRequest("GET", downloadUrl, nil)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	// ヘッダーを追加
+	req.Header.Add("Accept", "application/json")
+	WINDY_API_KEY := os.Getenv("WINDY_API_KEY")
+
+	req.Header.Add("x-windy-api-key", WINDY_API_KEY)
+
+	res, err := http.DefaultClient.Do(req)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	defer res.Body.Close()
+	imageData, err := io.ReadAll(res.Body)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	if res.StatusCode != 200 {
+		fmt.Println("Status Code is " + strconv.Itoa(res.StatusCode))
+		return
+	}
+
+	return imageData
+
+}
---
+++ util/goGrpcClient.go
@@ -0,0 +1,66 @@
+package util
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"os"
+	"time"
+
+	pb "registWebCam/pb"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+)
+
+func CreateClient() (pb.EmbeddingServiceClient, error) {
+
+	conn, err := grpc.NewClient("localhost:50051", grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		log.Fatalf("Failed to connect: %v", err)
+	}
+
+	client := pb.NewEmbeddingServiceClient(conn)
+
+	return client, nil
+}
+
+func GetEmbedding(client pb.EmbeddingServiceClient, imageData []byte, filename string) (imgEmbeddng []float32) {
+
+	// 画像ファイルを読み込む
+	imageData, err := os.ReadFile(os.Args[1])
+	if err != nil {
+		log.Fatalf("Failed to read image file: %v", err)
+	}
+
+	// コンテキストを作成
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*30)
+	defer cancel()
+
+	// リクエストを作成して送信
+	req := &pb.ImageRequest{
+		ImageData: imageData,
+		Filename:  os.Args[1],
+	}
+
+	// 開始時間を取得
+	startTime := time.Now()
+	resp, err := client.GetEmbedding(ctx, req)
+	if err != nil {
+		log.Fatalf("Failed to get embedding: %v", err)
+	}
+
+	endTime := time.Now()
+	fmt.Printf("Elapsed time: %v\n", endTime.Sub(startTime))
+
+	if !resp.Success {
+		log.Fatalf("Server error: %s", resp.Error)
+	}
+
+	fmt.Printf("Successfully got embeddings of length: %d\n", len(resp.Embeddings))
+	if len(resp.Embeddings) >= 5 {
+		fmt.Println("First 5 embedding values:", resp.Embeddings[:5])
+	}
+
+	return resp.Embeddings
+}
